66c61b8be15e928e6691b1e904216d461a36f725
Valentyn N Sichkar <34546286+sichkar-valentyn@users.noreply.github.com>
2018-06-16 22:31:23 +0300

    Update operators.py

--- operators.py
+++ operators.py
@@ -17,9 +17,9 @@ print(op.mul(4, 5))
 print(op.contains([1, 2, 3], 4))
 
 x = [1, 2, 3]
-f = op.itemgetter(1)  # f(x) == x[1]
-print(f(x))
+f = op.itemgetter(1)  # f(x) == x[1] - creating function f using operator
+print(f(x))  # 2
 
 y = {'123': 3}
 f = op.itemgetter('123')
-print(f(y))
+print(f(y))  # 3
